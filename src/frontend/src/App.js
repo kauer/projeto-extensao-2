891094cdccb7911b00c75491c482dee93be803cc
Trighy <trighy3@gmail.com>
2025-10-21 22:01:31 -0300

    Começo do Site Deslogado.

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import logo from './icone.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>HEADER</p>
       </header>
+      <a>TutorIA - Um Tutor Virtual</a>
+      <img src={logo} className="TutorIA-logo" alt="logo" />
+      <p></p>
+      <button>Registrar</button>
+      <p></p>
+      <button>Logar</button>
     </div>
   );
 }
