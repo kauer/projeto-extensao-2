cdc48498590e79bf5ad6668c5fa67e2a140d8acb
kauer <kauedossantosrocha@gmail.com>
2025-10-21 21:57:15 -0300

    Update do projeto. Inicialização dos testes com banco de dados

--- src/backend/main.py
+++ src/backend/main.py
@@ -3,6 +3,9 @@ from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
-async def read_root():
+def read_root():
     return {"message": "Hello World"}
-uv
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
